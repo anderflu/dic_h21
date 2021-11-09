b4bbdfba0426e315cdb5e9310826aade1b2b9bd5
Anders <anders.lundberg@live.no>
2021-11-09 14:40:38 +0100

    small changes

--- project/verilog/pixelArray.v
+++ project/verilog/pixelArray.v
@@ -1,1 +1,13 @@
 // Module for the 2 x 2 pixel array using pixelSensor
+
+module pixelArray;
+
+
+    logic [7:0] pixArr [0:1][0:1];
+
+
+
+
+
+
+endmodule
--- project/verilog/pixelState.v
+++ project/verilog/pixelState.v
@@ -1,1 +1,26 @@
 // State machine using pixelArray
+// This should control RESET, EXPOSURE, ADC and READOUT of the pixelarray
+
+
+   always_ff @(negedge clk ) begin
+      case(state)
+        STATE1: begin
+           erase <= 1;
+           read <= 0;
+           expose <= 0;
+           convert <= 0;
+        end
+        STATE2: begin
+           erase <= 0;
+           read <= 0;
+           expose <= 1;
+           convert <= 0;
+        end
+        STATE3: begin
+           erase <= 0;
+           read <= 0;
+           expose <= 0;
+           convert = 1;
+        end
+      endcase // case (state)
+   end // always @ (state)
